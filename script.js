6a26306c56fd9f20fc2053df8fbb08462886d340
“Dmitry” <frg3289@yandex.ru>
2024-02-17 22:05:16 +0300

    finish basic app

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // code here
 
+const form = document.querySelector('form');
+const input = document.querySelector('#name'); // обращение к инпуту через индентификатор
+const message = document.querySelector('#message');
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    const name = input.value;
+    message.textContent = `${name}, это твоё первое приложение`;
+});
